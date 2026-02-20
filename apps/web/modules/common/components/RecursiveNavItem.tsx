0153d0b2517390ae3a3504105e776c92e68cd9c5
Jorge Serrano <entrevistasmias@gmail.com>
2026-02-20 16:06:01 -0300

    Visual fixes

--- apps/web/modules/common/components/RecursiveNavItem.tsx
+++ apps/web/modules/common/components/RecursiveNavItem.tsx
@@ -9,11 +9,13 @@ interface RecursiveNavItemProps {
 }
 
 export const RecursiveNavItem = ({ item, depth = 0 }: RecursiveNavItemProps) => {
-    const arrowClass = cn("icon w-4 h-4 group-hover:rotate-180 transition-transform ml-2 text-xs", depth === 0 ? "text-white" : "text-ot-orange");
+    const arrowClass = cn("icon w-4 h-4 rotate-0 group-hover:rotate-180 transition-transform ml-2 text-xs", 
+        depth === 0 ? "text-white" : "text-ot-orange");
 
     return (
         <li className={cn("relative text-sm py-2 cursor-pointer group hover:[&>a>div>svg]:text-ot-orange hover:[&>ul]:block hover:[&>a>div>span.icon]:rotate-90 uppercase font-semibold",
-            (depth > 1 || (item.subMenu && item.subMenu.length > 0)) && "hover:bg-ot-light-blue")}>
+            (depth > 1 || (item.subMenu && item.subMenu.length > 0)) && "hover:bg-ot-light-blue/90",
+            depth > 1 && "bg-linear-to-r from-ot-dark-blue/30 to-ot-light-blue/70")}>
             <Link href={item.href} className="block w-full h-full px-4 py-2 text-gray-400" prefetch>
                 <div className="flex justify-between items-center">
                     <span className={`group-hover:text-ot-orange ${depth === 0 ? "text-white" : "text-ot-orange"}`}>{item.name}</span>
@@ -24,7 +26,7 @@ export const RecursiveNavItem = ({ item, depth = 0 }: RecursiveNavItemProps) => 
             </Link>
             {item.subMenu && item.subMenu.length > 0 && (
                 <ul
-                    className={cn("absolute hidden text-ot-orange! shadow-lg w-64 z-50 bg-ot-dark-blue",
+                    className={cn("absolute hidden text-ot-orange! shadow-lg w-64 z-50 bg-ot-light-blue",
                         depth === 0 ? "top-full left-0" : "top-0 left-full",
                     )}
                 >
