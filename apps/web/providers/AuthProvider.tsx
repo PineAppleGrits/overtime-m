a26da0a660776955596daa4c3a1dcaf507b25eb1
Jorge Serrano <entrevistasmias@gmail.com>
2026-03-12 17:20:20 -0300

    Fix auth

--- apps/web/providers/AuthProvider.tsx
+++ apps/web/providers/AuthProvider.tsx
@@ -6,17 +6,15 @@ import { useRouter } from 'next/navigation';
 
 const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in env var
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel
-    (typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000');
-  
-  // Make sure to include `https://` when not localhost
-  url = url.includes('http') ? url : `https://${url}`;
-  // Make sure trailing slash is removed
-  url = url.charAt(url.length - 1) === '/' ? url.slice(0, -1) : url;
-  
-  return url;
-};
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith('/') ? url : `${url}/`
+  return url
+}
 
 export type Profile = {
   id: string;
