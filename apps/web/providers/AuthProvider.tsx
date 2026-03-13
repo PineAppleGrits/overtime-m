1cfb5631786fba730efd3183c806018c189d2e12
Jorge Serrano <entrevistasmias@gmail.com>
2026-03-13 12:32:53 -0300

    URL Logs

--- apps/web/providers/AuthProvider.tsx
+++ apps/web/providers/AuthProvider.tsx
@@ -5,14 +5,14 @@ import { createClient } from '@/lib/supabase/client';
 import { useRouter } from 'next/navigation';
 
 const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/'
+  console.log("NEXT_PUBLIC_SITE_URL", process.env.NEXT_PUBLIC_SITE_URL)
+  console.log("NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL)
+  let url = process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/'
   // Make sure to include `https://` when not localhost.
   url = url.startsWith('http') ? url : `https://${url}`
   // Make sure to include a trailing `/`.
   url = url.endsWith('/') ? url : `${url}/`
+  console.log("URL FINAL", url)
   return url
 }
 
@@ -89,7 +89,7 @@ export function AuthProvider({
   const signInWithGoogle = async () => {
     setLoading(true);
     const url = getURL();
-    console.log("URL VERCEL", url);
+
     try {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
